f2a1d5a5e1a983ab2e36ca8c8dc4c7f556743894
takumashibata <westside_storydoor@yahoo.co.jp>
2021-08-11 05:07:14 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  #root to: 'tasks#index'
   
   resources :tasks
 end
